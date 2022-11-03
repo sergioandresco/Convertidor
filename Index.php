5f58e709167c80d64f19b93c4a8ed0ce886406f7
Sergio Cobos <43445037+sergioandresco@users.noreply.github.com>
2022-11-03 11:31:33 -0500

    Commit 2 Sergio

--- Index.php
+++ Index.php
@@ -11,6 +11,7 @@
 <body>
     <header style="background-color: skyblue;">
     <center>
+        <h1>Hola esto es una prueba</h1>
         <h1>Convertidor de Monedas</h1>
     </center>
     </header>
